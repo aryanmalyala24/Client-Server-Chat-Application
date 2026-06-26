c74124da4569719ad5afd8df59f87a5985aa37eb
aryanmalyala24 <aryanmalyala.24@kgpian.iitkgp.ac.in>
2026-06-26 16:52:53 +0530

    removed some unwanted libraries

--- client.c
+++ client.c
@@ -1,60 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <time.h>
 #include <netdb.h>
 
 
 void error(const char *msg){
     perror(msg);
-    exit(1);
+    exit(0);
 }
 
-/*
-argv[0] -> filename
-argv[1] -> server_ipaddress
-argv[2] -> portno
-*/
-
 int main(int argc, char *argv[]){
-    char buffer[255];
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
+    
+    char buffer[256];
     if(argc < 3){
         fprintf(stderr, "Usage %s hostname port\n", argv[0]);
-        exit(1);
+        exit(0);
     }
 
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if(sockfd < 0) error("ERROR opening socket\n");
-
-    printf("1. SockFD: %d\n", sockfd);
+    if(sockfd < 0)
+        error("ERROR opening socket\n");
 
     server = gethostbyname(argv[1]);
-    if(server == NULL) fprintf(stderr, "Error, no such host\n");
+    if(server == NULL) 
+        fprintf(stderr, "Error, no such host\n");
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(portno);
     bcopy((char *)server->h_addr_list[0], (char *) &serv_addr.sin_addr.s_addr, server->h_length);
 
-    if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) error("Connection Failed\n");
-    printf("2. SockFD: %d\n", sockfd);
+    if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        error("Connection Failed\n");
+        
     while(1){
         bzero(buffer, 255);
         fgets(buffer, 255, stdin);
--- server.c
+++ server.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <time.h>
 
 void error(const char *msg){
     perror(msg);
@@ -34,9 +26,8 @@ int main(int argc, char *argv[]){
     socklen_t clilen;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0){
+    if(sockfd < 0)
         error("Error opening socket\n");
-    }
 
     
     bzero((char *)&serv_addr, sizeof(serv_addr));
@@ -46,30 +37,34 @@ int main(int argc, char *argv[]){
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
-    if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("Binding Failed\n");
-    }
 
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
 
     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-    if(newsockfd < 0) error("Error on Accept\n");
-    printf("SockFD: %d\n", newsockfd);
+    if(newsockfd < 0) 
+        error("Error on Accept\n");
+        
     while(1){
         bzero(buffer, 255);
         n = read(newsockfd, buffer, 255);
-        if(n < 0) error("Error on reading\n");
+        if(n < 0) 
+            error("Error on reading\n");
 
         printf("Client: %s", buffer);
         bzero(buffer, 255);
         fgets(buffer, 255, stdin); 
+
         n = write(newsockfd, buffer, strlen(buffer));
-        if(n < 0) error("Error on writing\n");
+        if(n < 0) 
+            error("Error on writing\n");
 
         int i = strncmp("Bye", buffer, 3);
-        if(i == 0) break;
+        if(i == 0) 
+            break;
     }
 
     close(newsockfd);
