7c2f0befe50bb1412ff3eeac708ce429ac74aa1a
aryanmalyala24 <aryanmalyala.24@kgpian.iitkgp.ac.in>
2026-06-26 19:29:34 +0530

    updating to windows

--- client.c
+++ client.c
@@ -1,63 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
+#pragma comment(lib, "ws2_32.lib")
 
-void error(const char *msg){
-    perror(msg);
-    exit(0);
+void error(const char *msg)
+{
+    fprintf(stderr, "%s Error Code: %d\n", msg, WSAGetLastError());
+    WSACleanup();
+    exit(1);
 }
 
-int main(int argc, char *argv[]){
-    int sockfd, portno, n;
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: %s hostname port\n", argv[0]);
+        return 1;
+    }
+
+    WSADATA wsa;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        printf("WSAStartup Failed.\n");
+        return 1;
+    }
+
+    SOCKET sockfd;
+    int portno, n;
+
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    
+
     char buffer[256];
-    if(argc < 3){
-        fprintf(stderr, "Usage %s hostname port\n", argv[0]);
-        exit(0);
-    }
 
     portno = atoi(argv[2]);
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0)
-        error("ERROR opening socket\n");
+
+    if (sockfd == INVALID_SOCKET)
+        error("Socket creation failed.");
 
     server = gethostbyname(argv[1]);
-    if(server == NULL) 
-        fprintf(stderr, "Error, no such host\n");
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(portno);
-    bcopy((char *)server->h_addr_list[0], (char *) &serv_addr.sin_addr.s_addr, server->h_length);
-
-    if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-        error("Connection Failed\n");
-        
-    while(1){
-        bzero(buffer, 255);
-        fgets(buffer, 255, stdin);
-        n = write(sockfd, buffer, strlen(buffer));
-        if(n < 0) error("Error on writing\n");
-        
-        bzero(buffer, 255);
-        n = read(sockfd, buffer, 255);
-        if(n < 0) error("Error on reading\n");
-        printf("Server: %s", buffer);
-
-        int i = strncmp("Bye", buffer, 3);
-        if(i == 0) break;
+    if (server == NULL)
+    {
+        printf("No such host.\n");
+        WSACleanup();
+        return 1;
     }
 
-    close(sockfd);
+    memset(&serv_addr, 0, sizeof(serv_addr));
 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+
+    memcpy(&serv_addr.sin_addr.s_addr,
+           server->h_addr_list[0],
+           server->h_length);
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr,
+                sizeof(serv_addr)) == SOCKET_ERROR)
+        error("Connection failed.");
+
+    printf("Connected to server.\n");
+
+    while (1)
+    {
+        memset(buffer, 0, sizeof(buffer));
+
+        printf("You: ");
+        fgets(buffer, sizeof(buffer), stdin);
+
+        n = send(sockfd, buffer, strlen(buffer), 0);
+
+        if (n == SOCKET_ERROR)
+            error("Send failed.");
+
+        if (strncmp(buffer, "Bye", 3) == 0)
+            break;
+
+        memset(buffer, 0, sizeof(buffer));
+
+        n = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+
+        if (n <= 0)
+            break;
+
+        buffer[n] = '\0';
+
+        printf("Server: %s", buffer);
+
+        if (strncmp(buffer, "Bye", 3) == 0)
+            break;
+    }
+
+    closesocket(sockfd);
+    WSACleanup();
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,74 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
-void error(const char *msg){
-    perror(msg);
+#pragma comment(lib, "ws2_32.lib")
+
+void error(const char *msg)
+{
+    fprintf(stderr, "%s Error Code: %d\n", msg, WSAGetLastError());
+    WSACleanup();
     exit(1);
 }
 
-int main(int argc, char *argv[]){
-    if(argc < 2){
-        fprintf(stderr, "Port not provided. Program terminated.\n");
-        exit(1);
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Port not provided.\n");
+        return 1;
     }
 
-    
-    int sockfd, newsockfd, portno, n;
-    char buffer[255];
+    WSADATA wsa;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        printf("WSAStartup Failed.\n");
+        return 1;
+    }
+
+    SOCKET sockfd, newsockfd;
+    int portno, n;
+
+    char buffer[256];
 
     struct sockaddr_in serv_addr, cli_addr;
-    socklen_t clilen;
+    int clilen;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0)
-        error("Error opening socket\n");
 
-    
-    bzero((char *)&serv_addr, sizeof(serv_addr));
+    if (sockfd == INVALID_SOCKET)
+        error("Socket creation failed.");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+
     portno = atoi(argv[1]);
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
-    if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        error("Binding Failed\n");
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR)
+        error("Binding failed.");
 
-    listen(sockfd, 5);
+    if (listen(sockfd, 5) == SOCKET_ERROR)
+        error("Listen failed.");
+
+    printf("Server started on port %d\n", portno);
+    printf("Waiting for client...\n");
+
     clilen = sizeof(cli_addr);
 
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
 
-    if(newsockfd < 0) 
-        error("Error on Accept\n");
-        
-    while(1){
-        bzero(buffer, 255);
-        n = read(newsockfd, buffer, 255);
-        if(n < 0) 
-            error("Error on reading\n");
+    if (newsockfd == INVALID_SOCKET)
+        error("Accept failed.");
+
+    printf("Client connected.\n");
+
+    while (1)
+    {
+        memset(buffer, 0, sizeof(buffer));
+
+        n = recv(newsockfd, buffer, sizeof(buffer) - 1, 0);
+
+        if (n <= 0)
+            break;
+
+        buffer[n] = '\0';
 
         printf("Client: %s", buffer);
-        bzero(buffer, 255);
-        fgets(buffer, 255, stdin); 
 
-        n = write(newsockfd, buffer, strlen(buffer));
-        if(n < 0) 
-            error("Error on writing\n");
+        memset(buffer, 0, sizeof(buffer));
 
-        int i = strncmp("Bye", buffer, 3);
-        if(i == 0) 
+        fgets(buffer, sizeof(buffer), stdin);
+
+        n = send(newsockfd, buffer, strlen(buffer), 0);
+
+        if (n == SOCKET_ERROR)
+            error("Send failed.");
+
+        if (strncmp(buffer, "Bye", 3) == 0)
             break;
     }
 
-    close(newsockfd);
-    close(sockfd);
+    closesocket(newsockfd);
+    closesocket(sockfd);
+
+    WSACleanup();
 
     return 0;
 }
